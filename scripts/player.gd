a8da67c0339b3e69548647fd7ae3164e7926e2de
emeraldmoonx <emeraldmoonx@gmail.com>
2024-08-22 15:21:06 -0400

    Animations fixed
    Platforms work, animations have been fixed so jump now works fine.

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,10 @@ extends CharacterBody2D
 @export var gravity: float = 1000.0
 @export var jump_force: float = -300.0
 
+var is_jumping: bool = false
+var jump_timer: float = 0.0
+@export var min_air_time: float = 0.1  # Minimum time in the air before checking if landed
+
 func _physics_process(delta):
 	# Apply gravity to the built-in velocity property
 	velocity.y += gravity * delta
@@ -12,21 +16,39 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		velocity.x = speed
 		$AnimatedSprite2D.flip_h = false
-		$AnimatedSprite2D.play("run")
+		if is_on_floor() and !is_jumping:
+			$AnimatedSprite2D.play("run")
 	elif Input.is_action_pressed("move_left"):
 		velocity.x = -speed
 		$AnimatedSprite2D.flip_h = true
-		$AnimatedSprite2D.play("run")
+		if is_on_floor() and !is_jumping:
+			$AnimatedSprite2D.play("run")
 	else:
 		velocity.x = 0
-		$AnimatedSprite2D.play("idle")
+		if is_on_floor() and !is_jumping:
+			$AnimatedSprite2D.play("idle")
 	
-	# Jumping logic (now roll)
+	# Jumping logic
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_force
-	# Ensure roll animation continues mid-air
-	if !is_on_floor():
-		$AnimatedSprite2D.play("jumping")
-	
+		is_jumping = true
+		jump_timer = 0.0  # Reset the jump timer
+		$AnimatedSprite2D.play("jumping")  # Play the jump animation
+
+	# Update jump timer while in the air
+	if is_jumping and !is_on_floor():
+		jump_timer += delta
+		if $AnimatedSprite2D.animation != "jumping":
+			$AnimatedSprite2D.play("jumping")
+
+	# Check if the player has landed after the minimum air time
+	if is_on_floor():
+		if jump_timer > min_air_time:
+			is_jumping = false
+		if velocity.x == 0:
+			$AnimatedSprite2D.play("idle")
+		else:
+			$AnimatedSprite2D.play("run")
+
 	# Move and slide the player
 	move_and_slide()
